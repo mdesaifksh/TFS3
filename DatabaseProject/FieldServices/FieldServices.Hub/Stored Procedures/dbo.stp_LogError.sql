28ba7dfded6c58ece10022bf0a92f96cd9d09243
Brandon Setegn <BSetegn@firstkeyhomes.com>
2018-11-06 17:18:48 -0500

    Add stored procedures for logging Errors.

---
+++ DatabaseProject/FieldServices/FieldServices.Hub/Stored Procedures/dbo.stp_LogError.sql
@@ -0,0 +1,33 @@
+﻿USE HUB
+GO
+
+Create or Alter PROCEDURE dbo.stp_LogError
+	 @LogLevel AS Int
+	,@SprocName as VARCHAR(200)
+	,@Message AS VARCHAR(Max)
+	,@AppName as VARCHAR(200) = 'Hub'	
+	,@DetailsJson AS VARCHAR(MAX) = null
+	,@User AS VARCHAR(100) = null
+	,@MachineName AS VARCHAR(100) = @@SERVERNAME
+	,@LogDate As Datetime = null
+AS
+
+BEGIN 
+ 
+DECLARE @ErrorNumber INT = ERROR_NUMBER();
+DECLARE @ErrorLine INT = ERROR_LINE();
+DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE();
+DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
+DECLARE @ErrorState INT = ERROR_STATE();
+
+Declare @Exception Varchar(5000)
+Set @Exception = 
+	'Number:' + Convert(varchar(200), @ErrorNumber) 
+	+ '; Line:' + Convert(varchar(200), @ErrorLine)
+	+ '; Severity:' + Convert(varchar(200), @ErrorSeverity)
+	+ '; State:' + Convert(varchar(200), @ErrorState)
+	+ '; Message:' + @ErrorMessage;
+
+exec stp_LogMessage @LogLevel, @SprocName, @Message, @AppName, @Exception, @DetailsJson, @User, @MachineName, @LogDate
+
+END
--- DatabaseProject/FieldServices/FieldServices.Hub/Stored Procedures/dbo.stp_LogMessage.sql
+++ DatabaseProject/FieldServices/FieldServices.Hub/Stored Procedures/dbo.stp_LogMessage.sql
@@ -1,16 +1,51 @@
 ﻿USE HUB
 GO
 
-Create or Alter PROCEDURE Hub.dbo.stp_LogMessage
+Create or Alter PROCEDURE dbo.stp_LogMessage
 	 @LogLevel AS Int
+	,@SprocName as VARCHAR(200)
 	,@Message AS VARCHAR(Max)
-
+	,@AppName as VARCHAR(200) = 'Hub'
+	,@Exception AS VARCHAR(Max) = null
+	,@DetailsJson AS VARCHAR(MAX) = null
+	,@User AS VARCHAR(100) = null
+	,@MachineName AS VARCHAR(100) = @@SERVERNAME
+	,@LogDate As Datetime = null
 AS
-BEGIN
-select * from [FileIndex] FI
-where
-	FI.BasePath like '%' + @BasePath 
-	AND FI.RelativeDirectory = @Directory
+
+BEGIN 
+
+SET @LogDate	= COALESCE(@LogDate,GETDATE())
+SET @User		= COALESCE(@User,SUSER_NAME())
+
+--Make sure Log Level passed in exists in LoggingLevels, otherwise default it to 4 (Info)
+select @LogLevel = ll.Id
+from hub.dbo.LoggingLevels ll
+where ll.Id = @LogLevel
+
+set @LogLevel = COALESCE(@LogLevel, 4)
+
+INSERT INTO [dbo].[Logging]
+           ([LogDate]
+           ,[AppName]
+           ,[SprocName]
+           ,[User]
+           ,[Level]
+           ,[MachineName]
+           ,[Message]
+           ,[Exception]
+           ,[DetailsJson])
+     VALUES
+           (@LogDate
+           ,@AppName
+           ,@SprocName
+           ,@User
+           ,@LogLevel
+           ,@MachineName
+           ,@Message
+           ,@Exception
+           ,@DetailsJson)
+
 END
 GO
 
