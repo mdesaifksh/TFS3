d97d33e19ae56bfa028c48f4dfd75c43d8713736
Malhar Desai <malhar.desai@neudesic.com>
2018-11-06 11:13:06 -0800

    Commiting my latest changes.

---
+++ D365Test/Constants.cs
@@ -0,0 +1,47 @@
+﻿
+namespace D365Test
+{
+    public class Constants
+    {
+        public const string TARGET = "Target";
+        public const string POST_IMAGE = "PostImage";
+        public const string ENTITY_REFERENCE = "entityReference";
+        public const string IS_SUCCESS = "IsSuccess";
+        public const string ERROR_MESSAGE = "ErrorMessage";
+
+        public class Messages
+        {
+            public const string Create = "Create";
+            public const string Update = "Update";
+            public const string Win = "Win";
+            public const string SetState = "SetState";
+            public const string SetStateDynamic = "SetStateDynamicEntity";
+        }
+
+        public class Status
+        {
+            public const string StatusCode = "statuscode";
+            public const string StateCode = "statecode";
+        }
+
+        public class ProjectTasks
+        {
+            public const string LogicalName = "msdyn_projecttask";
+            public const string PrimaryKey = "msdyn_projecttaskid";
+
+            public const string Project = "msdyn_project";
+            public const string TaskIdentifier = "fkh_taskidentifierid";
+            public const string Subject = "msdyn_subject";
+            public const string WBSID = "msdyn_wbsid";
+        }
+
+        public class Projects
+        {
+            public const string LogicalName = "msdyn_project";
+            public const string PrimaryKey = "msdyn_projectid";
+
+            public const string ProjectTemplate = "msdyn_projecttemplate";
+        }
+
+    }
+}
---
+++ D365Test/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace D365Test
+{
+    class Program
+    {
+        private static CrmServiceClient _client;
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                using (_client = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMConnectionString"].ConnectionString))
+                {
+                    //Do stuff
+                    WhoAmIResponse res = (WhoAmIResponse)_client.Execute(new WhoAmIRequest());
+                    Console.WriteLine($"Login User ID : {res.UserId}");
+                    Console.WriteLine($"Organization Unique Name : {_client.ConnectedOrgUniqueName}");
+                    Console.WriteLine($"Organization Display Name : {_client.ConnectedOrgFriendlyName}");
+                    //Entity test = RetrieveProjectTemplateTask(new EntityReference("msdyn_project", new Guid("23A38E60-C0D0-E811-A96E-000D3A16ACEE")), "1");
+
+                    Entity ProjectEntity = new Entity("msdyn_project");
+                    ProjectEntity["fkh_unitid"] = new EntityReference("po_unit", new Guid("6068F309-8A9D-E811-A857-000D3A14019B"));
+                    ProjectEntity["msdyn_projecttemplate"] = new EntityReference("msdyn_project", new Guid("23A38E60-C0D0-E811-A96E-000D3A16ACEE"));
+                    ProjectEntity["msdyn_subject"] = "Malhar Test Project 2";
+
+                    ProjectEntity.Id = _client.Create(ProjectEntity);
+
+
+                    Console.WriteLine($"Press any key to exit.");
+                    Console.Read();
+                }
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                string message = ex.Message;
+                throw;
+            }
+        }
+
+        private static Entity RetrieveProjectTemplateTask(EntityReference projectTemplateEntityReference, string WBSID)
+        {
+            Console.WriteLine($"Retrieve Project Template Task with WBSID : {WBSID}.");
+
+            QueryExpression Query = new QueryExpression
+            {
+                EntityName = Constants.ProjectTasks.LogicalName,
+                ColumnSet = new ColumnSet(true),
+                Criteria = new FilterExpression
+                {
+                    FilterOperator = LogicalOperator.And,
+                    Conditions =
+                    {
+                        new ConditionExpression
+                        {
+                            AttributeName = Constants.ProjectTasks.WBSID,
+                            Operator = ConditionOperator.Equal,
+                            Values = { WBSID }
+                        },
+                        new ConditionExpression
+                        {
+                            AttributeName = Constants.ProjectTasks.TaskIdentifier,
+                            Operator = ConditionOperator.NotNull
+                        },
+                        new ConditionExpression
+                        {
+                            AttributeName = Constants.ProjectTasks.Project,
+                            Operator = ConditionOperator.Equal,
+                            Values = { projectTemplateEntityReference.Id }
+                        }
+                    }
+                },
+                TopCount = 1
+            };
+
+            EntityCollection projectTaskEntityCollection = _client.RetrieveMultiple(Query);
+            if (projectTaskEntityCollection.Entities.Count > 0)
+            {
+                Console.WriteLine($"Project Template Task found.");
+                return projectTaskEntityCollection.Entities[0];
+            }
+            else
+            {
+                Console.WriteLine($"Project Template Task NOT found.");
+                return null;
+            }
+        }
+
+    }
+}
