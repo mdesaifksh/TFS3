fb084bb039eaba20e817f933f6ca5d45fb350c05
Brandon Setegn <BSetegn@firstkeyhomes.com>
2018-12-17 19:10:22 -0500

    Added Stored Procs for Move Out Date Changed and Yardi Contract\Job created.

--- DatabaseProject/FieldServices/FieldServices.Hub/Stored Procedures/FS_UnitStatus_NoticeSent.sql
+++ DatabaseProject/FieldServices/FieldServices.Hub/Stored Procedures/FS_UnitStatus_NoticeSent.sql
@@ -11,7 +11,7 @@ CREATE OR ALTER PROCEDURE [dbo].FS_UnitStatus_NoticeSent
 AS
 BEGIN
 
-	SET NOCOUNT ON;
+SET NOCOUNT ON;
 
 	
 declare @SprocName varchar(100) = 'FS_UnitStatus_NoticeSent'
@@ -30,8 +30,10 @@ with recent_event as (
 )
 , yardi_data as (
 select 
+top 3
 	datediff(day, yd.UnitStatusBegin, getdate()) as DaysSinceStatusChange 
 	,yd.YardiId
+	,yd.YardiScode
 	,yd.UnitStatus
 	,yd.UnitStatusBegin
 	,re.LastEventDate
@@ -43,24 +45,25 @@ yd.UnitStatus in ('Notice Unrented','Notice Rented','Vacant Unrented Not Ready',
 and UnitStatusBegin <= convert(date, getdate()) and datediff(day, UnitStatusBegin, getdate()) < 15
 and LastEventDate is null
 )
-   --INSERT INTO hub.dbo.eventlog 
-   --               (event_id, 
-   --                source_id, 
-   --                load_date, 
-   --                voyager_property_hmy, 
-   --                json_payload) 
+   INSERT INTO hub.dbo.eventlog 
+                  (event_id, 
+                   source_id, 
+                   load_date, 
+                   voyager_property_hmy, 
+				   voyager_property_scode,
+                   json_payload) 
       SELECT @EventID                          AS Event_ID, 
              @SourceID                         AS Source_ID, 
              Getdate()                         AS Load_Date, 
              YardiId, 
+			 YardiScode,
 			 (select 
 				UnitStatusBegin as Date1
 				,@EventID as [Event]
-				,yardiid  as PropertyID
+				,YardiScode  as PropertyID
 				,CAST(0 as BIT) as IsForce
 				FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) as Json_Payload
         FROM   yardi_data 
-
 		
 declare @message varchar(200) = 'Finished Inserting Records into  hub.dbo.eventlog : Count:' + format(@@ROWCOUNT, 'N0');
 exec Hub.dbo.stp_LogMessage @LogLevel=3, @SprocName = @SprocName, @Message = @message;
