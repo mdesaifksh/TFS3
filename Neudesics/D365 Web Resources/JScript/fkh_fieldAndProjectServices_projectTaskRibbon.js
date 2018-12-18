d24212ddaf31208fbe55f087bf85d7b272e647e8
Malhar Desai <MDesai@firstkeyhomes.com>
2018-12-18 05:50:03 +0000

    Updated fkh_fieldAndProjectServices_projectTaskRibbon.js to work on Job Completed related task.

--- Neudesics/D365 Web Resources/JScript/fkh_fieldAndProjectServices_projectTaskRibbon.js
+++ Neudesics/D365 Web Resources/JScript/fkh_fieldAndProjectServices_projectTaskRibbon.js
@@ -1,16 +1,16 @@
 if (typeof (FKH) == "undefined") {
     FKH = { __namespace: true };
 }
-if (typeof (FKH.FieldAndProjectServices) == "undefined") {
+if (typeof (FKH.FieldAndProjectServices) === "undefined") {
     FKH.FieldAndProjectServices = {};
 }
 FKH.FieldAndProjectServices.ProjectTaskRibbon = {
 
     onClick_MarkComplete: function () {
-        if (Xrm.Page.getAttribute("msdyn_subject") != null && Xrm.Page.getAttribute("msdyn_subject").getValue() != null){
+        if (Xrm.Page.getAttribute("msdyn_subject") != null && Xrm.Page.getAttribute("msdyn_subject").getValue() != null) {
             var thisTaskName = Xrm.Page.getAttribute("msdyn_subject").getValue();
             var thisProjectId = Xrm.Page.getAttribute("msdyn_project").getValue()[0].id;
-            switch(thisTaskName) {
+            switch (thisTaskName) {
                 case 'Pre-Move-Out Inspection':
                     FKH.FieldAndProjectServices.ProjectTaskRibbon.completeThisTask();
                     Xrm.Page.data.refresh(true);
@@ -38,7 +38,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
                     break;
                 case 'Work In Progress':
                     FKH.FieldAndProjectServices.ProjectTaskRibbon.completeThisTask();
-                    FKH.FieldAndProjectServices.ProjectTaskRibbon.getTasks(thisTaskName, thisProjectId, ['Vendor Says Job\'s Complete','Quality Control Inspection']);
+                    FKH.FieldAndProjectServices.ProjectTaskRibbon.getTasks(thisTaskName, thisProjectId, ['Vendor Says Job\'s Complete', 'Quality Control Inspection']);
                     Xrm.Page.data.refresh(true);
                     break;
                 case 'Quality Control Inspection':
@@ -48,7 +48,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
                     break;
                 case 'Job Completed':
                     FKH.FieldAndProjectServices.ProjectTaskRibbon.completeThisTask();
-                    FKH.FieldAndProjectServices.ProjectTaskRibbon.getTasks(thisTaskName, thisProjectId, ['Marketing Inspection','Quality Control Inspection']);
+                    FKH.FieldAndProjectServices.ProjectTaskRibbon.getTasks(thisTaskName, thisProjectId, ['Marketing Inspection', 'Quality Control Inspection']);
                     FKH.FieldAndProjectServices.ProjectTaskRibbon.publishMessage(thisTaskName);
                     Xrm.Page.data.refresh(true);
                     break;
@@ -81,13 +81,13 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
         Xrm.Page.getAttribute("msdyn_progress").setValue(100);
         Xrm.Page.getAttribute("statuscode").setValue(963850001);//Completed
     },
-    
+
     isVisible_MarkComplete: function () {
-        if (Xrm.Page.getAttribute("msdyn_subject") != null && Xrm.Page.getAttribute("msdyn_subject").getValue() != null){
+        if (Xrm.Page.getAttribute("msdyn_subject") != null && Xrm.Page.getAttribute("msdyn_subject").getValue() != null) {
             var taskName = Xrm.Page.getAttribute("msdyn_subject").getValue();
             var taskStatus = Xrm.Page.getAttribute("statuscode").getValue();
             if (taskStatus != 963850001) {//Completed
-                switch(taskName) {
+                switch (taskName) {
                     case 'Pre-Move-Out Inspection':
                     case 'Move-Out Inspection':
                     case 'Budget Start':
@@ -99,6 +99,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
                     case 'Marketing Inspection':
                     case 'Bi-Weekly Inspection':
                     case 'Move-In Inspection':
+                    case 'Job Completed':
                         return true;
                     default:
                         return false;
@@ -108,10 +109,10 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
         return false;
     },
 
-    getTasks: function(thisTaskName, projectId, targetTaskNames){
+    getTasks: function (thisTaskName, projectId, targetTaskNames) {
         var targetTaskNameFilter = "msdyn_subject eq '" + targetTaskNames[0] + "'";
         if (targetTaskNames.length > 1) {
-            for (i = 1; i < targetTaskNames.length; i++) { 
+            for (i = 1; i < targetTaskNames.length; i++) {
                 targetTaskNameFilter += " or msdyn_subject eq '" + targetTaskNames[i] + "'";
             }
         }
@@ -122,7 +123,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
         req.setRequestHeader("Accept", "application/json");
         req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
         req.setRequestHeader("Prefer", "odata.include-annotations=\"*\"");
-        req.onreadystatechange = function() {
+        req.onreadystatechange = function () {
             if (this.readyState === 4) {
                 req.onreadystatechange = null;
                 if (this.status === 200) {
@@ -135,8 +136,8 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
         req.send();
     },
 
-    processTasks: function(thisTaskName, results){
-        switch(thisTaskName) {
+    processTasks: function (thisTaskName, results) {
+        switch (thisTaskName) {
             case 'Move-Out Inspection':
             case 'Budget Start':
             case 'Budget Approval':
@@ -145,7 +146,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
                 break;
             case 'Work In Progress':
                 for (var i = 0; i < results.value.length; i++) {
-                    switch(results.value[i]["msdyn_subject"]) {
+                    switch (results.value[i]["msdyn_subject"]) {
                         case 'Vendor Says Job\'s Complete':
                             FKH.FieldAndProjectServices.ProjectTaskRibbon.completeTask(results.value[i]);
                         case 'Quality Control Inspection':
@@ -156,7 +157,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
                 break;
             case 'Job Completed':
                 for (var i = 0; i < results.value.length; i++) {
-                    switch(results.value[i]["msdyn_subject"]) {
+                    switch (results.value[i]["msdyn_subject"]) {
                         case 'Quality Control Inspection':
                             FKH.FieldAndProjectServices.ProjectTaskRibbon.completeTask(results.value[i]);
                         case 'Marketing Inspection':
@@ -174,7 +175,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
         }
     },
 
-    startTask: function(task) {
+    startTask: function (task) {
         if (task["statuscode"] == 1) { //Not Started
             var entity = {};
             entity.msdyn_actualstart = new Date().toISOString();
@@ -182,12 +183,12 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
             entity.statuscode = 963850000; //In Progress
 
             var req = new XMLHttpRequest();
-            req.open("PATCH", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/msdyn_projecttasks("+ task["msdyn_projecttaskid"] +")", true);
+            req.open("PATCH", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/msdyn_projecttasks(" + task["msdyn_projecttaskid"] + ")", true);
             req.setRequestHeader("OData-MaxVersion", "4.0");
             req.setRequestHeader("OData-Version", "4.0");
             req.setRequestHeader("Accept", "application/json");
             req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-            req.onreadystatechange = function() {
+            req.onreadystatechange = function () {
                 if (this.readyState === 4) {
                     req.onreadystatechange = null;
                     if (this.status === 204) {
@@ -201,7 +202,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
         }
     },
 
-    completeTask: function(task) {
+    completeTask: function (task) {
         if (task["statuscode"] != 963850001) { //Completed
             var entity = {};
             if (task["msdyn_actualstart"] == null) {
@@ -212,12 +213,12 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
             entity.statuscode = 963850001; //Completed
 
             var req = new XMLHttpRequest();
-            req.open("PATCH", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/msdyn_projecttasks("+ task["msdyn_projecttaskid"] +")", true);
+            req.open("PATCH", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/msdyn_projecttasks(" + task["msdyn_projecttaskid"] + ")", true);
             req.setRequestHeader("OData-MaxVersion", "4.0");
             req.setRequestHeader("OData-Version", "4.0");
             req.setRequestHeader("Accept", "application/json");
             req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-            req.onreadystatechange = function() {
+            req.onreadystatechange = function () {
                 if (this.readyState === 4) {
                     req.onreadystatechange = null;
                     if (this.status === 204) {
@@ -231,15 +232,60 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
         }
     },
 
-    createReWorkTasks: function(projectId) {
+    createReWorkTasks: function (projectId) {
 
     },
-    
+
     publishMessage: function (thisTaskName) {
+        debugger;
+        var unit = Xrm.Page.getAttribute('fkh_unitid').getValue();
+        var today = new Date();
+        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        var dateTime = date + ' ' + time;
+
+        if (unit !== null) {
+            Xrm.WebApi.retrieveRecord("po_units", unit[0].id.replace('{', '').replace('}', ''), "?$select=po_unitid,po_unitidnum").then(
+                function success(result) {
+                    if (result.po_unitidnum !== null && result.po_unitidnum !== '' && result.po_unitidnum !== undefined) {
+                        //alert(result.po_unitidnum);
+
+                        var data =
+                        {
+                            "fkh_eventdata": "[{'id': '" + Createguid() + "', 'eventType': 'allEvents', 'subject': 'Turn Process : JOB_COMPLETED', 'eventTime': '" + dateTime + "', 'data': { 'PropertyID': '" + result.po_unitidnum + "', 'Job': '<need to define>', 'Contract': '<need to define>', 'Event': 17, 'Date1': '" + dateTime + "'}, 'Topic': '' }]",
+                            "fkh_direction": true,
+                            "fkh_name": "Turn Process : JOB_COMPLETED"
+                        };
+
+                        // create account record
+                        Xrm.WebApi.createRecord("fkh_azureintegrationcalls", data).then(
+                            function success(result) {
+                                console.log("Azure Integration Call created with ID: " + result.id);
+                                // perform operations on record creation
+                            },
+                            function (error) {
+                                console.log(error.message);
+                                alert(error.message);
+                                // handle error conditions
+                            }
+                        );
+
+                    }
+
+                    //console.log(`Retrieved values: Name: ${result.name}, Revenue: ${result.revenue}`);
+                    // perform operations on record retrieval
+                },
+                function (error) {
+                    Xrm.Utility.alertDialog(error.message);
+                    // handle error conditions
+                }
+            );
+        }
+        /*
         var entity = {};
         entity.fkh_direction = true;
-        entity.fkh_eventdata = "[{   'Id': '<need to define>',   'EventType': '<need to define>',   'Subject': 'Dynamics: Job Complete',   'EventTime': '<need to define>',   'Data': {     'Property': '<need to define>',     'Job': '<need to define>',     'Contract': '<need to define>',     'Event': 'Job Complete'   },   'dataVersion': '',   'metadataVersion': '1',   'Topic': '<need to define>' }]";
-        entity.fkh_name = "Dynamics: Job Complete";
+        entity.fkh_eventdata = "[{   'id': '" + Createguid() + "',   'eventType': 'allEvents',   'subject': 'Turn Process : JOB_COMPLETED',   'eventTime': '<need to define>',   'data': {     'PropertyID': '<need to define>',     'Job': '<need to define>',     'Contract': '<need to define>',     'Event': 'Job Complete'   },   'dataVersion': '',   'metadataVersion': '1',   'Topic': '' }]";
+        entity.fkh_name = "Turn Process : JOB_COMPLETED";
 
         var req = new XMLHttpRequest();
         req.open("POST", Xrm.Page.context.getClientUrl() + "/api/data/v9.1/fkh_azureintegrationcalls", true);
@@ -247,7 +293,7 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
         req.setRequestHeader("OData-Version", "4.0");
         req.setRequestHeader("Accept", "application/json");
         req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
-        req.onreadystatechange = function() {
+        req.onreadystatechange = function () {
             if (this.readyState === 4) {
                 req.onreadystatechange = null;
                 if (this.status === 204) {
@@ -259,9 +305,20 @@ FKH.FieldAndProjectServices.ProjectTaskRibbon = {
                     Xrm.Utility.alertDialog(this.statusText);
                 }
             } else {
+                alert('Error');
             }
         };
         Xrm.Utility.alertDialog("Marking job as complete in all systems.");
         req.send(JSON.stringify(entity));
+        */
     }
+};
+
+function Createguid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
