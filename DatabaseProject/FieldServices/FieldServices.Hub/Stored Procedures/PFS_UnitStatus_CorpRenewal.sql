2c98b7fe27c879b263ffcf6d7f751f9e0c7ceb87
Brandon Setegn <BSetegn@firstkeyhomes.com>
2018-12-17 19:31:51 -0500

    Updated logic for UnitStatus Notice Sent.

--- DatabaseProject/FieldServices/FieldServices.Hub/Stored Procedures/PFS_UnitStatus_CorpRenewal.sql
+++ DatabaseProject/FieldServices/FieldServices.Hub/Stored Procedures/PFS_UnitStatus_CorpRenewal.sql
@@ -7,44 +7,42 @@ Resident Notice to Move Out - Field Services Event Fire
 Description:	This procedure will write a record to the Hub.dbo.EventLog table when a property's
 				unit status changes to the designated status.
 *******************************************************************************************/
-CREATE OR ALTER PROCEDURE [dbo].FS_UnitStatus_NoticeSent
+CREATE OR ALTER PROCEDURE [dbo].PFS_UnitStatus_CorpRenewal
 AS
 BEGIN
 
 SET NOCOUNT ON;
 
 	
-declare @SprocName varchar(100) = 'FS_UnitStatus_NoticeSent'
-exec Hub.dbo.stp_LogMessage @LogLevel=4, @SprocName = @SprocName, @Message ='Checking Unit Status for Notice Sent'
+declare @SprocName varchar(100) = 'PFS_UnitStatus_CorpRenewal'
+exec Hub.dbo.stp_LogMessage @LogLevel=4, @SprocName = @SprocName, @Message ='Checking Unit Status for Corporate Renewal Complete'
+
 
 Declare @EventId int = 1;
+Declare @PrevEventId int = 1;
 Declare @SourceId int = 1;
 
-with recent_event as (
+with create_event as (
 	SELECT 
 		voyager_property_hmy 
 		,max(load_date) as LastEventDate
     FROM   hub.dbo.eventlog 
-	WHERE Event_ID = @EventId
+	WHERE Event_ID = @PrevEventId
 	GROUP BY Voyager_Property_HMY
 )
 , yardi_data as (
 select 
-top 3
-	datediff(day, yd.CurrentUnitStatusBegin, getdate()) as DaysSinceStatusChange 
-	,yd.YardiId
+top 3	
+	yd.YardiId
 	,yd.YardiScode
-	,yd.UnitStatus
-	,yd.CurrentUnitStatusBegin
-	,re.LastEventDate
+	,yd.UnitStatus	
+	,ce.LastEventDate
 from 
 Homes.dbo.vYardi_PropertyLevel_YD yd
-LEFT JOIN recent_event re on re.Voyager_Property_HMY = yd.YardiId
+LEFT JOIN create_event ce on ce.Voyager_Property_HMY = yd.YardiId
 where 
-yd.UnitStatus in ('Notice Unrented','Notice Rented','Vacant Unrented Not Ready','Vacant Rented Not Ready')
---Either the unit status begin date is null or the event happened recently
-and (CurrentUnitStatusBegin  is null OR (CurrentUnitStatusBegin <= convert(date, getdate()) and datediff(day, CurrentUnitStatusBegin, getdate()) < 15))
-and LastEventDate is null
+yd.UnitStatus in ('Vacant Rented Ready','Vacant Not Rented Ready','Occupied No Notice','Down')
+and LastEventDate is not null
 )
    INSERT INTO hub.dbo.eventlog 
                   (event_id, 
@@ -59,14 +57,14 @@ and LastEventDate is null
              YardiId, 
 			 YardiScode,
 			 (select 
-				isnull(CurrentUnitStatusBegin, convert(date,getdate())) as Date1
+				UnitStatusBegin as Date1
 				,@EventID as [Event]
 				,YardiScode  as PropertyID
 				,CAST(0 as BIT) as IsForce
 				FOR JSON PATH, WITHOUT_ARRAY_WRAPPER) as Json_Payload
         FROM   yardi_data 
 		
-declare @message varchar(200) = 'Finished Inserting Records into  hub.dbo.eventlog : Count:' + format(@@ROWCOUNT, 'N0');
+declare @message varchar(200) = 'Finished Inserting Records for Corp Renewal Complete into  hub.dbo.eventlog : Count:' + format(@@ROWCOUNT, 'N0');
 exec Hub.dbo.stp_LogMessage @LogLevel=3, @SprocName = @SprocName, @Message = @message;
 
 END
