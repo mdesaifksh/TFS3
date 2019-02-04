67b6bd0f2d3e886c368eecb6356163841dfd9781
Brandon Setegn <BSetegn@firstkeyhomes.com>
2019-02-04 14:46:47 -0500

    Renamed tables to prepend PFS_

--- DatabaseProject/FieldServices/FieldServices.Hub/Tables/dbo.EventIncomingLog.sql
+++ DatabaseProject/FieldServices/FieldServices.Hub/Tables/dbo.EventIncomingLog.sql
@@ -3,19 +3,21 @@ GO
 
 --drop  TABLE [dbo].[EventIncomingLog]
 
-CREATE TABLE [dbo].[EventIncomingLog](
+CREATE TABLE [dbo].[PFS_EventIncomingLog](
 	[ID] [int] IDENTITY(1,1) NOT NULL,
 	Topic varchar(500) NOT NULL,
 	Message_Subject varchar(500) NOT NULL,
 	Message_Sent datetime not null,	
 	[Json_Payload] [varchar](max) NULL,
-	CreatedAt datetime not null default(getdate())	
+	CreatedAt datetime not null default(getdate()),
+	[RowVersion] [timestamp] NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
 
-ALTER TABLE EventIncomingLog
+/*
+ALTER TABLE PFS_EventIncomingLog
 ADD [RowVersion] [timestamp] NOT NULL
-
+*/
 --select * from [EventIncomingLog]
 
